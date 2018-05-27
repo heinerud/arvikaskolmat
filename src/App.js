1c13f8beb7b65edd16a1408b6e2a57ee66a014a6
Joel Heinerud <joel.heinerud@gmail.com>
2018-05-27 18:25:26 +0200

    Gray out days that have passed

--- src/App.js
+++ src/App.js
@@ -20,10 +20,13 @@ class Day extends Component {
       marginTop: '0px',
       fontWeight: 'normal'
     }
+    const today = new Date()
+    let dayHasPassed = today > new Date(Number(this.props.date))
+    let fontColor = dayHasPassed ? 'gray' : 'black'
 
     return (
-      <div style={margins}>
-        <h3 style={{ marginBottom: '5px' }}>{this.props.day}</h3>
+      <div style={{ margins, color: fontColor }} >
+        <h3 style={{ fontWeight: 'normal', marginBottom: '5px' }}>{this.props.day}</h3>
         <p style={dishStyle}>{this.props.dish}</p>
       </div>
     );
@@ -51,7 +54,7 @@ class App extends Component {
           .map(x => x.MenuPresentation)
           .map(x => {
             return {
-              day: dayOfWeek(Number(x.DayMenuDate)),
+              date: x.DayMenuDate,
               info: x.DayMenuInfo,
               dish: x.DayMenuName,
             }
@@ -63,8 +66,9 @@ class App extends Component {
   render() {
     let menu = this.state.menu ?
       this.state.menu.map(x => {
-        let day = x.info.length > 0 ? x.day + ' (' + x.info + ')' : x.day
-        return { day: day, dish: x.dish }
+        let dayName = dayOfWeek(Number(x.date))
+        let day = x.info.length > 0 ? dayName + ' (' + x.info + ')' : dayName
+        return { date: x.date, dish: x.dish, day: day }
       }) : []
     let title = this.state.title ? this.state.title + ' (v. ' + this.state.currentWeek + ')' : ''
 
@@ -73,7 +77,7 @@ class App extends Component {
         {this.state.menu ?
           <div style={margins}>
             <h2 style={normalFont}>{title}</h2>
-            {menu.map((x, i) => <Day day={x.day} dish={x.dish} key={i} />)}
+            {menu.map((x, i) => <Day day={x.day} date={x.date} dish={x.dish} key={i} />)}
           </div>
           : <div style={margins}><h2 style={normalFont}>Fetching menu...</h2></div>
         }
