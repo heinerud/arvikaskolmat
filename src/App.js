d23b9aec7f16aa292cd59b56d34ed4609bce6cfc
Joel Heinerud <joel.heinerud@gmail.com>
2018-05-27 14:30:59 +0200

    Use better data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
 
-function dayOfWeek(dateStr) {
+function dayOfWeek(dateNumber) {
   const days = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag']
-  let date = new Date(dateStr)
+  let date = new Date(dateNumber)
   let dayIndex = date.getDay()
   return days[Number(dayIndex) - 1]
-}
-
-Date.prototype.getWeek = function () {
-  var date = new Date(this.getTime());
-  date.setHours(0, 0, 0, 0);
-  // Thursday in current week decides the year.
-  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-  // January 4 is always in week 1.
-  var week1 = new Date(date.getFullYear(), 0, 4);
-  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-    - 3 + (week1.getDay() + 6) % 7) / 7);
-}
-
-function date2Week(dateStr) {
-  let date = new Date(dateStr)
-  return date.getWeek()
 }
 
 class Day extends Component {
   render() {
     return (
       <div style={{ marginLeft: '1%' }}>
-        <h2>{this.props.date}</h2>
+        <h2>{this.props.day}</h2>
         <h3 style={{ marginLeft: '3%' }}>{this.props.dish}</h3>
       </div >
     );
@@ -39,36 +22,48 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: null,
+      currentWeek: null,
       menu: null,
     }
   }
   componentDidMount() {
     fetch(this.props.url)
-      .then(response => response.json())
-      .then(data => this.setState({
-        title: data.title,
-        menu: data.menu
+      .then(res => res.json())
+      .then(res => this.setState({
+        currentWeek: res.CurrentWeek,
+        title: res.Weeks
+          .filter(x => x.WeekNumber === res.CurrentWeek)[0].MenuName,
+        menu: res.Weeks
+          .filter(x => x.WeekNumber === res.CurrentWeek)[0].Days
+          .map(x => x.DayMenus[0])
+          .map(x => x.MenuPresentation)
+          .map(x => {
+            return {
+              day: dayOfWeek(Number(x.DayMenuDate)),
+              info: x.DayMenuInfo,
+              dish: x.DayMenuName,
+            }
+          })
       }))
+      .then(x => console.log(this.state))
   }
 
   render() {
-    let now = new Date()
-    let currentWeek = date2Week(now)
-    let daysToRender =
-      this.state.menu ?
-        this.state.menu
-          .filter(menuItem => date2Week(menuItem.date) === currentWeek)
-        : []
+    let menu = this.state.menu ?
+      this.state.menu.map(x => {
+        let day = x.info.length > 0 ? x.day + ' (' + x.info + ')' : x.day
+        return { day: day, dish: x.dish }
+      }) : []
+    let title = this.state.title ? this.state.title + ' (v. ' + this.state.currentWeek + ')' : ''
 
     return (
       <div className="App">
-        {this.state.title ?
+        {this.state.menu ?
           <div>
-            <h1 style={{ marginLeft: '1%' }}>{this.state.title}</h1>
-            {daysToRender.map((x, i) => <Day date={dayOfWeek(x.date)} dish={x.dish} key={i} />)}
+            <h1 style={{ marginLeft: '1%' }}>{title}</h1>
+            {menu.map((x, i) => <Day day={x.day} dish={x.dish} key={i} />)}
           </div>
-          : <div><h1>Loading...</h1></div>
+          : <div><h1>Fetching menu...</h1></div>
         }
       </div>
     );
