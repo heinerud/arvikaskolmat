5e9b1bc6de35f113a308b929e742e3c06327fff5
Joel Heinerud <joel.heinerud@gmail.com>
2018-08-31 17:50:05 +0200

    Move client app to /client

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 import { Route } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 
-let api = 'https://skolmat-api.herokuapp.com';
-
 ReactDOM.render(<BrowserRouter>
     <div>
-        <Route path='/school' render={() => <App url={api + '/school'} />} />
-        <Route path='/preschool' render={() => <App url={api + '/preschool'} />} />
+        <Route path='/school' render={() => <App url={'api/school'} />} />
+        <Route path='/preschool' render={() => <App url={'api/preschool'} />} />
     </div>
 </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
